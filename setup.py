c9f046890e038c84952702f865d02f1de68b496b
Penumbrage <wwangVer2.0@gmail.com>
2022-07-10 04:52:18 -0600

    added some dependencies to the setup.py file

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='single_tb9051ftg_rpi',
       version='1.0.0',
       description=('Library for the Pololu Single TB9051FTG Motor '
                    'Driver for Raspberry Pi'),
       url='https://www.pololu.com/product/2997',
+      install_requires=[
+            'pigpio'
+      ],
       py_modules=['single_tb9051ftg_rpi'],
       )
